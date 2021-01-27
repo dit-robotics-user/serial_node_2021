c49d8517f090ec574d7a67a66f67bd811e21ee51
dit-robotics <60845896+dit-robotics@users.noreply.github.com>
2021-01-27 22:17:33 +0800

    final version

--- src/serial_port.cpp
+++ src/serial_port.cpp
@@ -38,13 +38,11 @@ uint32_t modified_crc32_mpeg_2(uint8_t *data, uint8_t length){
     uint32_t crc = 0xffffffff;  // Initial value
     int8_t l = length;
 
-    while(length--)
-    {	
-		crc ^= (uint32_t)(*(j+ data++)) << 24;
+    while(length--){
+        crc ^= (uint32_t)(*(j+ data++)) << 24;
         j-=2;
         if(j == -5) j=3;
-        for (i = 0; i < 8; ++i)
-        {
+        for (i = 0; i < 8; ++i){
             if ( crc & 0x80000000 )
                 crc = (crc << 1) ^ 0x04C11DB7;
             else
@@ -60,10 +58,10 @@ void write_callback(const std_msgs::String::ConstPtr &msg){
 
 void fromAgent_callback(const std_msgs::Int32MultiArray::ConstPtr &msg){
     for(int i = 1; i < tx_len; i++){
-		tx[i] = msg->data[i-1];
-	}
-    tx[0] = 0x31; //ST1:0x31,ST2:0x32
-	tx[tx_len] = modified_crc32_mpeg_2((uint8_t*)tx, 4*tx_len);
+        tx[i] = msg->data[i-1];
+    }
+    tx[0] = 0x31;//ST1:0x31,ST2:0x32
+    tx[tx_len] = modified_crc32_mpeg_2((uint8_t*)tx, 4*tx_len);
 }
 
 int main(int argc, char **argv){
@@ -71,7 +69,6 @@ int main(int argc, char **argv){
     ros::NodeHandle nh;
     // ros::Subscriber write_sub = nh.subscribe("write", 1000, write_callback);
     // ros::Publisher write_pub = nh.advertise<std_msgs::String>("write", 1000);
-    
     ros::Subscriber from_agent = nh.subscribe("txST1", 1, fromAgent_callback);
     ros::Publisher read_pub = nh.advertise<std_msgs::Int32MultiArray>("rxST1", 1);
     ros::Publisher rate_pub = nh.advertise<std_msgs::Int32>("success_rate", 1000);
@@ -83,10 +80,9 @@ int main(int argc, char **argv){
         ser.setTimeout(to);
         ser.open();
     }
-    
     catch(serial::IOException &e){
         ROS_ERROR_STREAM("Unable to open port ");
-        return -1;                                                                       
+        return -1;
     }
 
     if(ser.isOpen()){
@@ -100,40 +96,39 @@ int main(int argc, char **argv){
    
     std_msgs::Int32MultiArray rx_msg;
     string test;
-	std_msgs::Int32 success_rate_msg;
+    std_msgs::Int32 success_rate_msg;
     std_msgs::String tx_str;
 
     while(ros::ok()){
-		bool flag;
+        bool flag;
         if(ser.available()>=4*(rx_len+2+1)){
             test = ser.readline(4*(rx_len+2+1), ">?");
-			rx_msg.data.clear();
-			for(int i=0; i<rx_len+1+1; i++){
-				tmp = (int32_t)test[4*i] + (int32_t)test[4*i+1]*256 + (int32_t)test[4*i+2]*65536 +(int32_t)test[4*i+3]*16777216;
-				indata[i] = tmp;
-			}
-			
-			// crc ckeck
-			rcv_count ++;
-			if(indata[rx_len] == modified_crc32_mpeg_2((uint8_t*)indata, 4*rx_len)){
+            rx_msg.data.clear();
+            for(int i=0; i<rx_len+1+1; i++){
+                tmp = (int32_t)test[4*i] + (int32_t)test[4*i+1]*256 + (int32_t)test[4*i+2]*65536 +(int32_t)test[4*i+3]*16777216;
+                indata[i] = tmp;
+            }
+            // crc ckeck
+            rcv_count ++;
+            if(indata[rx_len] == modified_crc32_mpeg_2((uint8_t*)indata, 4*rx_len)){
                 for(int i = 1; i < rx_len; i++){
                     rx_msg.data.push_back(indata[i]);
                 }
                 read_pub.publish(rx_msg);
                 if(indata[rx_len + 1] == 255) flag = 1;
                 else if(indata[rx_len + 1] == 0) flag = 0;
-			}
-			else{
-			    error_count++;
-			}
-			
-			if(rcv_count >= 100){
-			    success_rate = (int)(100*(rcv_count-error_count)/rcv_count);
+            }
+            else{
+                error_count++;
+            }
+
+            if(rcv_count >= 100){
+                success_rate = (int)(100*(rcv_count-error_count)/rcv_count);
                 success_rate_msg.data = success_rate;
                 rate_pub.publish(success_rate_msg);
-			    rcv_count = 0;
-			    error_count = 0;
-			}
+                rcv_count = 0;
+                error_count = 0;
+            }
         }
 
         // serial transmit 
